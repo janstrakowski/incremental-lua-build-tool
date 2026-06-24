b1ff111e7d85a7a8c533e5453c7e3cfafe9bf986
Jan Strakowski <janstrakowski@proton.me>
2026-06-24 20:33:54 +0200

    remove .zig-cache

--- .zig-cache/c/cb20dc577db6372950391b9aabd9c70b/options.zig
+++
@@ -1,5 +0,0 @@
-pub const enable_tracy: bool = false;
-pub const enable_tracy_callstack: bool = false;
-pub const enable_tracy_allocation: bool = false;
-pub const debug_allocations: bool = false;
-pub const version_str: []const u8 = "0.0.0";
